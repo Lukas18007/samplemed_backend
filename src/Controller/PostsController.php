40d1b82f8bec2d8303005b7de1823c95afae834c
Lukas_leandro <leandrolukas916@gmail.com>
2024-08-11 12:56:07 -0300

    Creating posts controller

---
+++ src/Controller/PostsController.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Controller;
+use Cake\View\JsonView;
+
+class PostsController extends AppController
+{
+    public function viewClasses(): array
+    {
+        return [JsonView::class];
+    }
+
+    public function index()
+    {
+        $posts = $this->Posts->find('all')->all();
+        $this->set('posts', $posts);
+        $this->viewBuilder()->setOption('serialize', ['posts']);
+    }
+
+    public function view($id)
+    {
+        $post = $this->Posts->get($id);
+        $this->set('post', $post);
+        $this->viewBuilder()->setOption('serialize', ['post']);
+    }
+
+    public function add()
+    {
+        $this->request->allowMethod(['post']);
+        $post = $this->Posts->newEntity($this->request->getData());
+
+        $message =  $this->Posts->save($post) ?'Saved' : 'Error';
+
+        $this->set([
+            'message' => $message,
+            'post' => $post,
+        ]);
+        $this->viewBuilder()->setOption('serialize', ['post', 'message']);
+    }
+
+    public function edit($id)
+    {
+        $this->request->allowMethod(['patch', 'put']);
+
+        $post = $this->Posts->get($id);
+        $post = $this->Posts->patchEntity($post, $this->request->getData());
+
+        $message =  $this->posts->save($post) ?'Saved' : 'Error';
+
+        $this->set([
+            'message' => $message,
+            'post' => $post,
+        ]);
+
+        $this->viewBuilder()->setOption('serialize', ['post', 'message']);
+    }
+
+    public function delete($id)
+    {
+        $this->request->allowMethod(['delete']);
+        $post = $this->Posts->get($id);
+        $message = 'Deleted';
+        if (!$this->Posts->delete($post)) {
+            $message = 'Error';
+        }
+        $this->set('message', $message);
+        $this->viewBuilder()->setOption('serialize', ['message']);
+    }
+}
