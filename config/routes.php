09ca8dceb00105c2cdac943c70196ced923464f1
Lukas_leandro <leandrolukas916@gmail.com>
2024-08-11 16:53:40 -0300

    Simplifying routes

--- config/routes.php
+++ config/routes.php
@@ -6,18 +6,7 @@ return function (RouteBuilder $routes): void {
     $routes->scope('/api', function ($routes) {
         $routes->setExtensions(['json']);
 
-        $routes->connect('/posts', ['controller' => 'Posts', 'action' => 'index', '_method' => 'GET']);
-        
-        $routes->connect(
-            '/posts/:id', 
-            ['controller' => 'Posts', 'action' => 'view', '_method' => 'GET']
-        )->setPass(['id'])->setPatterns(['id' => '\d+']);
-
-        $routes->connect('/posts', ['controller' => 'Posts', 'action' => 'add', '_method' => 'POST']);
-        
-        $routes->connect(
-            '/posts/:id', 
-            ['controller' => 'Posts', 'action' => 'delete', '_method' => 'DELETE']
-        )->setPass(['id'])->setPatterns(['id' => '\d+']);
+        $routes->setExtensions(['json']);
+        $routes->resources('Posts');
     });
 };
